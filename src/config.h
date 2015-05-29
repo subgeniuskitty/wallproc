318d65282c462919e097577d2eb02fc8e7f0cbe9
Aaron Taylor <ataylor@subgeniuskitty.com>
2015-05-28 20:38:35 -0700

    Added makefile for easier building.
    Added config.h with keybindings. Updated README to reflect filename.

---
+++ src/config.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+
+/*
+ * =====================================================================================================================
+ * keybindings
+ * =====================================================================================================================
+ */
+
+/* 
+ * To change keybindings, change the corresponding #define to the desired SDL keycode.
+ * SDL keycode reference located at: https://wiki.libsdl.org/SDL_Keycode
+ */
+
+#define KEY_NEXT SDLK_PERIOD                    // Using period since it's the same key as '>', a forward arrow.
+#define KEY_PREV SDLK_COMMA                     // Using comma since it's the same key as '<', a back arrow.
+
+#define KEY_SIZEUP SDLK_KP_PLUS                 // Increase selection box size
+#define KEY_SIZEDOWN SDLK_KP_MINUS              // Decrease selection box size
+
+                                                // Move selection box:
+#define KEY_UP SDLK_KP_8                        //   Up
+#define KEY_DOWN SDLK_KP_2                      //   Down
+#define KEY_LEFT SDLK_KP_4                      //   Left
+#define KEY_RIGHT SDLK_KP_6                     //   Right
+
+#define KEY_SELECTIONBOX_RESET SDLK_KP_0        // Reset size and location of selection box to defaults
+
+#define KEY_QUIT SDLK_q                         // Exit this application
+#define KEY_HELP SDLK_h                         // Pops up a dialog box with key commands after the GUI has launched. 
+#define KEY_TOGGLE_OUTLINE_COLOR SDLK_KP_5      // Toggles the selection box color between light and dark.
+
